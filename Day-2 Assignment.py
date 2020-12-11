2bf86fc95fd371a5a612ae3f5144a38d8af67180
AjithNetha <75791103+AjithNetha@users.noreply.github.com>
2020-12-11 20:23:34 +0530

    Add files via upload

---
+++ Day-2 Assignment.py
@@ -0,0 +1,53 @@
+print("-------LetsUpgrade Day-2-------")
+print("\n# Assignment-1 : string functions")
+string = "NomulaAjithNetha"
+print("string = ",string)
+print("string.endswith('a') : ",string.endswith("a"))
+print("string.startswith('n') : ",string.startswith("n"))
+print("len(string) :              ",len(string))
+print("string.upper() :         ",string.upper())
+print("string.islower() :       ",string.islower())
+print("string.isalpha() :       ",string.isalpha())
+print("string.count('Nomula') : ",string.count("Nomula"))
+print("string.isalnum():       ",string.isalnum())
+print("string.capitalize() :    ",string.capitalize())
+print("string.center(50) :     ",string.center(50))
+print("string.isdigit() :         ",string.isdigit())
+
+print("\n# Assignment-2 : list inbuilt methods")
+a=["Ajith",22,2/4,12.8]
+b=[1,2,3,4]
+print("b=",b)
+b.clear()
+print("b.clear() : ",b)
+a.append(23)
+print("a.append(23) :",a)
+a.reverse()
+print("a.reverse() :    ",a)
+b=a.copy()
+print("b=a.copy() :    ",b)
+b.pop()
+print("b.pop() :        ",b)
+b.sort()
+print("b.sort() :        ",b) 
+
+print("\n# Assignment-3 = dictionary inbuilt methods")
+my_dict={"name":"Ajith","Age":23,"Study":"PostGraduation"}
+print("my_dict : ",my_dict)
+print(my_dict.keys())
+print(my_dict.values())
+print("len(my_dict) : ",len(my_dict))
+a=my_dict.copy()
+print("a=my_dict.copy() : ",a)
+my_dict.clear()
+print("my_dict.clear() : ",my_dict)
+
+
+
+
+
+
+
+
+
+
